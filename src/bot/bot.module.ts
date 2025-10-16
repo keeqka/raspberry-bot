919e54f5278d2a264476594a8f41361dcb0bad1f
Nikita <108035712+sobakasbakbaka@users.noreply.github.com>
2025-10-17 00:12:28 +0500

    Bot module was added. Also Docker configuration has been added

---
+++ src/bot/bot.module.ts
@@ -0,0 +1,25 @@
+import { Module, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Telegraf } from 'telegraf';
+
+@Module({})
+export class BotModule implements OnModuleInit {
+  private bot: Telegraf;
+
+  constructor(private configService: ConfigService) {}
+
+  async onModuleInit() {
+    const token = this.configService.get('BOT_TOKEN');
+    if (!token) {
+      throw new Error('BOT_TOKEN is not set');
+    }
+
+    this.bot = new Telegraf(token);
+
+    this.bot.start((ctx) => ctx.reply('Welcome!'));
+    this.bot.on('text', (ctx) => ctx.reply(ctx.message.text));
+
+    await this.bot.launch();
+    console.log('Bot started');
+  }
+}
